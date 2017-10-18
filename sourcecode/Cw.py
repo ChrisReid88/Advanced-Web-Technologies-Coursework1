a959cd335fa2238b73cdcfec4e76bfd32fa98825
ChrisReid88 <40202859@live.napier.ac.uk>
2017-10-18 18:27:24 +0100

    Routing semi working. Need to fix displaying individual movie details.

--- sourcecode/Cw.py
+++ sourcecode/Cw.py
@@ -4,11 +4,22 @@ app = Flask(__name__)
 
 
 @app.route('/')
-def getData():
+def home():
     with app.open_resource('static/data.json') as f:
         if __name__ == '__main__':
             data = json.load(f)
-        return render_template('body.html', response=data)
+        return render_template('home.html', response=data)
+
+@app.route('/about')
+def about():
+    return render_template('about.html')
+
+@app.route('/<name>')
+def movie(name=None):
+    with app.open_resource('static/data.json') as f:
+        if __name__ == '__main__':
+            data = json.load(f)
+        return render_template('movie.html', name=name, response=data)
 
 
 if __name__ ==  "__main__":
