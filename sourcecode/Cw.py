78824c07292a811e6a9337a276e838d9f0352c83
ChrisReid88 <40202859@live.napier.ac.uk>
2017-10-19 12:30:41 +0100

    Added 'Genres' page. Displays list of each genre from the json file. Routes properly to /genre/<genre>. Need to display results from filtered genres.

--- sourcecode/Cw.py
+++ sourcecode/Cw.py
@@ -22,6 +22,22 @@ def movie(name=None):
 
         return render_template('movie.html', name=name, response=data)
 
+@app.route('/genre')
+def genre():
+    with app.open_resource('static/data2.json') as f:
+        if __name__ == '__main__':
+            data = json.load(f)
+        genres=[]
+        for movie in data.values():
+            for genre in movie['genre']:
+                if genre not in genres:
+                    genres.append(genre)
+                    genres.sort()
+        return render_template('genre.html',gen=genres, response=data)
+
+# @app.route('/genre/<genre>')
+# def specificGenre():
+#
 
 if __name__ ==  "__main__":
     app.run(debug =True)
